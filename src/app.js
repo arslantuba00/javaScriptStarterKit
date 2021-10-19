e4ca23900d3c10c9e647d5ac62dfefe283edcef0
Tuba ARSLAN <arslanawzentuba1@gmail.com>
2021-10-19 23:07:15 +0300

    Javascript 1.ders kodu

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Merhaba televole")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+console.log(dolarBugun)
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+
+console.log("</ul>")
+// {
+//     <ul>
+//         <li>Konut kredisi</li>
+//         <li>Emlak Konut kredisi</li>
+//         <li>Kamu Konut kredisi</li>
+//     </ul>
+// }
